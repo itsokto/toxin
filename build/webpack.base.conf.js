78869ba6ea6f5d24be2c0ac113b7d2601aa16a60
itsokto <picnet9@gmail.com>
2020-12-17 05:24:30 +0300

    configure webpack for modular architecture

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -1,127 +1,151 @@
-const path = require('path')
-const fs = require('fs')
-const  MiniCssExtractPlugin  = require('mini-css-extract-plugin')
-const  CopyWebpackPlugin  = require('copy-webpack-plugin')
-const  HtmlWebpackPlugin  = require('html-webpack-plugin')
+const path = require("path");
+const glob = require("glob");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 // Main const
 // see more: https://github.com/vedees/webpack-template/blob/master/README.md#main-const
 const PATHS = {
-  src: path.join(__dirname, '../src'),
-  dist: path.join(__dirname, '../dist'),
-  assets: 'assets/'
-}
+  src: path.join(__dirname, "../src"),
+  dist: path.join(__dirname, "../dist"),
+  assets: "assets/",
+};
 
 // Pages const for HtmlWebpackPlugin
 // see more: https://github.com/vedees/webpack-template/blob/master/README.md#html-dir-folder
 // const PAGES_DIR = PATHS.src
-const PAGES_DIR = `${PATHS.src}/pug/pages/`
-const PAGES = fs.readdirSync(PAGES_DIR).filter(fileName => fileName.endsWith('.pug'))
+const PAGES_DIR = `${PATHS.src}/pug/pages/`;
+const PAGES = glob
+  .sync(PAGES_DIR + "**/*")
+  .filter((fileName) => fileName.endsWith(".pug"));
+
+function getEntries() {
+  return PAGES.reduce(
+    (a, page) => ({
+      ...a,
+      [path.basename(page).replace(/\.pug/, "")]: page.replace(/\.pug/, ".js"),
+    }),
+    {}
+  );
+}
 
 module.exports = {
   // BASE config
   externals: {
-    paths: PATHS
+    paths: PATHS,
   },
   entry: {
-    app: PATHS.src,
-    // module: `${PATHS.src}/your-module.js`,
+    ...getEntries(),
   },
   output: {
     filename: `${PATHS.assets}js/[name].[hash].js`,
     path: PATHS.dist,
-    publicPath: '/'
+    publicPath: "/",
   },
   optimization: {
+    runtimeChunk: "single", // enable "runtime" chunk
     splitChunks: {
       cacheGroups: {
         vendor: {
-          name: 'vendors',
-          test: /node_modules/,
-          chunks: 'all',
-          enforce: true
-        }
-      }
-    }
+          test: /[\\/]node_modules[\\/]/,
+          name: "common",
+          chunks: "all",
+        },
+      },
+    },
   },
   module: {
     rules: [
-    {
-      test: /\.pug$/,
-      loader: 'pug-loader'
-    },
-    {
-      test: /\.js$/,
-      loader: 'babel-loader',
-      exclude: '/node_modules/'
-    },
-    {
-      test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-      loader: 'file-loader',
-      options: {
-        name: '[name].[ext]'
-      }
-    }, 
-    {
-      test: /\.(png|jpg|gif|svg)$/,
-      loader: 'file-loader',
-      options: {
-        name: '[name].[ext]'
-      }
-    },
-    {
-      test: /\.scss$/,
-      use: [
-        'style-loader',
-        MiniCssExtractPlugin.loader,
-        {
-          loader: 'css-loader',
-          options: { sourceMap: true }
+      {
+        test: /\.pug$/,
+        loader: "pug-loader",
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "file-loader",
+        options: {
+          name: "[name].[ext]",
         },
-        {
-          loader: 'postcss-loader',
-          options: { sourceMap: true,  postcssOptions: { config: `./postcss.config.js` } }
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: "file-loader",
+        options: {
+          name: "[name].[ext]",
         },
-        'resolve-url-loader',
-        {
-          loader: 'sass-loader',
-          options: { sourceMap: true }
-        }
-      ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader",
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: { sourceMap: true },
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              sourceMap: true,
+              postcssOptions: { config: `./postcss.config.js` },
+            },
+          },
+          "resolve-url-loader",
+          {
+            loader: "sass-loader",
+            options: { sourceMap: true },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: { sourceMap: true },
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              sourceMap: true,
+              config: { path: `./postcss.config.js` },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      "~": PATHS.src,
     },
-    {
-      test: /\.css$/,
-      use: [
-        'style-loader',
-        MiniCssExtractPlugin.loader,
-        {
-          loader: 'css-loader',
-          options: { sourceMap: true }
-        }, {
-          loader: 'postcss-loader',
-          options: { sourceMap: true, config: { path: `./postcss.config.js` } }
-        }
-      ]
-    }]
   },
   plugins: [
     new MiniCssExtractPlugin({
       filename: `${PATHS.assets}css/[name].[hash].css`,
     }),
     new CopyWebpackPlugin({
-      patterns:[
-      { from: `${PATHS.src}/${PATHS.assets}img`, to: `${PATHS.assets}img` },
-      { from: `${PATHS.src}/${PATHS.assets}fonts`, to: `${PATHS.assets}fonts` },
-      { from: `${PATHS.src}/static`, to: '' },
-    ]
+      patterns: [
+        { from: `${PATHS.src}/${PATHS.assets}img`, to: `${PATHS.assets}img` },
+        {
+          from: `${PATHS.src}/${PATHS.assets}fonts`,
+          to: `${PATHS.assets}fonts`,
+        },
+        { from: `${PATHS.src}/static`, to: "" },
+      ],
     }),
 
     // Automatic creation any html pages (Don't forget to RERUN dev server)
-    // see more: https://github.com/vedees/webpack-template/blob/master/README.md#create-another-html-files
-    // best way to create pages: https://github.com/vedees/webpack-template/blob/master/README.md#third-method-best
-    ...PAGES.map(page => new HtmlWebpackPlugin({
-      template: `${PAGES_DIR}/${page}`,
-      filename: `./${page.replace(/\.pug/,'.html')}`
-    }))
+    ...PAGES.map(
+      (page) =>
+        new HtmlWebpackPlugin({
+          template: `${page}`,
+          chunks: [path.basename(page).replace(/\.pug/, ""), "common"],
+          filename: `${path.basename(page).replace(/\.pug/, ".html")}`,
+        })
+    ),
   ],
-}
+};
