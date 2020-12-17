a66c696963c7adca5475d70351ec2ba91dc5bc0f
itsokto <picnet9@gmail.com>
2020-12-17 23:35:06 +0300

    add header mixin and styles

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -1,141 +1,151 @@
-const path = require("path");
-const glob = require("glob");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path')
+const glob = require('glob')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 // Main const
 // see more: https://github.com/vedees/webpack-template/blob/master/README.md#main-const
 const PATHS = {
-  src: path.join(__dirname, "../src"),
-  dist: path.join(__dirname, "../dist"),
-  assets: "assets/",
-};
+  src: path.join(__dirname, '../src'),
+  dist: path.join(__dirname, '../dist'),
+  assets: 'assets/'
+}
 
 // Pages const for HtmlWebpackPlugin
 // see more: https://github.com/vedees/webpack-template/blob/master/README.md#html-dir-folder
 // const PAGES_DIR = PATHS.src
-const PAGES_DIR = `${PATHS.src}/pug/pages/`;
+const PAGES_DIR = `${PATHS.src}/pug/pages/`
 const PAGES = glob
-  .sync(PAGES_DIR + "**/*")
-  .filter((fileName) => fileName.endsWith(".pug"));
+  .sync(PAGES_DIR + '**/*')
+  .filter((fileName) => fileName.endsWith('.pug'))
 
 function getEntries() {
   return PAGES.reduce(
     (a, page) => ({
       ...a,
-      [path.basename(page).replace(/\.pug/, "")]: page.replace(/\.pug/, ".js"),
+      [path.basename(page).replace(/\.pug/, '')]: page.replace(/\.pug/, '.js')
     }),
     {}
-  );
+  )
 }
 
 module.exports = {
   // BASE config
   externals: {
-    paths: PATHS,
+    paths: PATHS
   },
   entry: {
-    ...getEntries(),
+    ...getEntries()
   },
   output: {
     filename: `${PATHS.assets}js/[name].[hash].js`,
     path: PATHS.dist,
-    publicPath: "/",
+    publicPath: '/'
   },
   optimization: {
-    runtimeChunk: "single", // enable "runtime" chunk
+    runtimeChunk: 'single', // enable "runtime" chunk
     splitChunks: {
       cacheGroups: {
         vendor: {
           test: /[\\/]node_modules[\\/]/,
-          name: "common",
-          chunks: "all",
-        },
-      },
-    },
+          name: 'common',
+          chunks: 'all'
+        }
+      }
+    }
   },
   module: {
     rules: [
       {
         test: /\.pug$/,
-        loader: "pug-loader",
+        loader: 'pug-loader'
       },
       {
         test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "file-loader",
+        loader: 'file-loader',
         options: {
-          name: "[name].[ext]",
-        },
+          name: '[name].[ext]'
+        }
       },
       {
         test: /\.(png|jpg|gif|svg)$/,
-        loader: "file-loader",
+        loader: 'file-loader',
         options: {
-          name: "[name].[ext]",
-        },
+          name: '[name].[ext]'
+        }
       },
       {
         test: /\.scss$/,
         use: [
-          "style-loader",
-          MiniCssExtractPlugin.loader,
+          'style-loader',
           {
-            loader: "css-loader",
-            options: { sourceMap: true },
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              esModule: false
+            }
           },
           {
-            loader: "postcss-loader",
+            loader: 'css-loader',
+            options: { sourceMap: true }
+          },
+          {
+            loader: 'postcss-loader',
             options: {
               sourceMap: true,
-              postcssOptions: { config: `./postcss.config.js` },
-            },
+              postcssOptions: { config: `./postcss.config.js` }
+            }
           },
-          "resolve-url-loader",
+          'resolve-url-loader',
           {
-            loader: "sass-loader",
-            options: { sourceMap: true },
-          },
-        ],
+            loader: 'sass-loader',
+            options: { sourceMap: true }
+          }
+        ]
       },
       {
         test: /\.css$/,
         use: [
-          "style-loader",
-          MiniCssExtractPlugin.loader,
+          'style-loader',
           {
-            loader: "css-loader",
-            options: { sourceMap: true },
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              esModule: false
+            }
           },
           {
-            loader: "postcss-loader",
+            loader: 'css-loader',
+            options: { sourceMap: true }
+          },
+          {
+            loader: 'postcss-loader',
             options: {
               sourceMap: true,
-              config: { path: `./postcss.config.js` },
-            },
-          },
-        ],
-      },
-    ],
+              config: { path: `./postcss.config.js` }
+            }
+          }
+        ]
+      }
+    ]
   },
   resolve: {
     alias: {
-      "~": PATHS.src,
-    },
+      '~': PATHS.src
+    }
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: `${PATHS.assets}css/[name].[hash].css`,
+      filename: `${PATHS.assets}css/[name].[hash].css`
     }),
     new CopyWebpackPlugin({
       patterns: [
         { from: `${PATHS.src}/${PATHS.assets}img`, to: `${PATHS.assets}img` },
         {
           from: `${PATHS.src}/${PATHS.assets}fonts`,
-          to: `${PATHS.assets}fonts`,
+          to: `${PATHS.assets}fonts`
         },
-        { from: `${PATHS.src}/static`, to: "" },
-      ],
+        { from: `${PATHS.src}/static`, to: '' }
+      ]
     }),
 
     // Automatic creation any html pages (Don't forget to RERUN dev server)
@@ -143,9 +153,9 @@ module.exports = {
       (page) =>
         new HtmlWebpackPlugin({
           template: `${page}`,
-          chunks: [path.basename(page).replace(/\.pug/, ""), "common"],
-          filename: `${path.basename(page).replace(/\.pug/, ".html")}`,
+          chunks: [path.basename(page).replace(/\.pug/, ''), 'common'],
+          filename: `${path.basename(page).replace(/\.pug/, '.html')}`
         })
-    ),
-  ],
-};
+    )
+  ]
+}
