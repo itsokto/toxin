289f30892fae9c6fec2e0fd0d4a095486b66c56f
itsokto <36706257+itsokto@users.noreply.github.com>
2021-01-25 22:09:56 +0300

    add masked text field

--- src/pug/pages/form-elements/form-elements.js
+++ src/pug/pages/form-elements/form-elements.js
@@ -3,3 +3,8 @@ import 'Components/input/input'
 import 'Components/checkbox-button/checkbox-button'
 import 'Components/toggle-button/toggle-button'
 import 'Components/radio-button/radio-button'
+import Inputmask from 'inputmask'
+
+$(function () {
+  Inputmask('99.99.9999').mask('.js-date-field')
+})
