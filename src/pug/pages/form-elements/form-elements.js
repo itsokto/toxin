96c4d3904e2555ae91246772adca68d5622878e8
itsokto <36706257+itsokto@users.noreply.github.com>
2021-01-30 16:03:17 +0300

    create rich-checkbox component

--- src/pug/pages/form-elements/form-elements.js
+++ src/pug/pages/form-elements/form-elements.js
@@ -4,6 +4,7 @@ import 'Components/checkbox-button/checkbox-button'
 import 'Components/toggle-button/toggle-button'
 import 'Components/radio-button/radio-button'
 import 'Components/bullet-list/bullet-list'
+import 'Components/rich-checkbox/rich-checkbox'
 
 import Inputmask from 'inputmask'
 
