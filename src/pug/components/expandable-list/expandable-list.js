c0d120e8cb749c169157882a6d0945a18d89ea35
itsokto <36706257+itsokto@users.noreply.github.com>
2021-02-02 21:59:50 +0300

    refactor expandable-list: simplify code

--- src/pug/components/expandable-list/expandable-list.js
+++ src/pug/components/expandable-list/expandable-list.js
@@ -1,20 +1,14 @@
 export default class ExpandableList {
   constructor(element) {
-    this.element = element
+    this.$element = element
     this.init()
   }
 
   init() {
-    this.element.each(function () {
-      const title = $(this).find('.js-expandable-list__title-container')
-      const dropdownMenu = $(this).find('.js-expandable-list__dropdown')
+    function toggleClass() {
+      $(this).toggleClass('expandable-list_active')
+    }
 
-      function toggleClass() {
-        $(title).toggleClass('expandable-list__title-container_active')
-        $(dropdownMenu).toggleClass('expandable-list__dropdown_active')
-      }
-
-      title.on('click', toggleClass)
-    })
+    this.$element.on('click', toggleClass)
   }
 }
